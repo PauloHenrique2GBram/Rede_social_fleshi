ffc9cfd2d41f012c18a4f51690ce2acd6bd9f3e2
Aluno <paulohenriquesilva301@gmail.com>
2025-11-19 16:47:59 -0300

    feat: initial setup of login and register

---
+++ appfleshi/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, length
+from appfleshi.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Nome de usuário', validators=[DataRequired(), length(min=2, max=20)])
+    password = PasswordField('Senha', validators=[DataRequired(), length(min=6, max=60)])
+    confirm_password = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Criar Conta')
+
+    def validate_email(self, field):
+        user = User.query.filter_by(email=field.data).first()
+        if user:
+            return ValidationError("E-mail já cadastrado. Por favor insira outro e-mail ou faça login.")
+        return None
